26cb4e534fe25c554fe3f3da1a7422b6c53f77cb
Max Bruckner <max@maxbruckner.de>
2021-08-18 01:32:51 +0200

    Mostly up til orphan rule

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 import 'reveal.js/dist/reveal.css';
 import 'reveal.js/dist/theme/black.css';
+import 'reveal.js/plugin/highlight/monokai.css'
 import Reveal from "reveal.js";
+import RevealNotes from "reveal.js/plugin/notes/notes.esm";
+import RevealHighlight from "reveal.js/plugin/highlight/highlight.esm";
+
+const highlightPlugin = RevealHighlight();
+highlightPlugin.hljs.configure({tabReplace: '  '})
 
 const deck = new Reveal();
-deck.initialize({hash: true, slideNumber: true})
+deck.initialize({
+	hash: true,
+	slideNumber: true,
+	plugins: [highlightPlugin, RevealNotes]
+})
